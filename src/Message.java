b29da7b1ff7067743229e9569533d5e95c359908
zhh <nenbaying@163.com>
2020-02-19 01:02:19 +0800

    message part question:    1.Why call Math.min() method to determine length of message array which need to be copied;    2.In writePartitionMessageToMessage() method why use this.offset to be the start index instead of this.offset+this.length.

--- src/Message.java
+++ src/Message.java
@@ -45,4 +45,15 @@ public class Message {
     public int writeToMessage(byte[] bytes) {
         return writeToMessage(bytes, 0, bytes.length);
     }
+
+    public int writePartitionMessageToMessage(Message message, int lastIdx) {
+        int startIdx = message.offset + lastIdx;
+        int len = message.length - (lastIdx - offset);
+        while (len > capacity - this.length) {
+            if (!messageBuffer.expandMessage(this))
+                return -1;
+        }
+        System.arraycopy(message.sharedArray, startIdx, message.sharedArray, this.offset + this.length, len);
+        return len;
+    }
 }
