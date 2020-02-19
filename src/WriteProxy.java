fed2e9cb6537a7258f03e9965e0b68048a193b07
zhh <nenbaying@163.com>
2020-02-19 16:01:43 +0800

    write proxy

--- src/WriteProxy.java
+++ src/WriteProxy.java
@@ -1,5 +1,22 @@
+import java.util.Queue;
+
 /**
- * @Author Honghan Zhu
+ * @author Honghan Zhu
  */
 public class WriteProxy {
+    private MessageBuffer messageBuffer = null;
+    private Queue<Message> writeQueue = null;
+
+    public WriteProxy(MessageBuffer messageBuffer, Queue<Message> writeQueue) {
+        this.messageBuffer = messageBuffer;
+        this.writeQueue = writeQueue;
+    }
+
+    public Message getMessage() {
+        return this.messageBuffer.getMessage();
+    }
+
+    public boolean enq(Message message) {
+        return writeQueue.offer(message);
+    }
 }
